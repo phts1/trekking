1e64cbef56183960baceed18a4c0f880f22a6ef8
phts1 <nsharp2111@gmail.com>
2019-01-27 13:41:25 +0300

    3

--- code.js
+++ code.js
@@ -1,103 +1,41 @@
-let checkbox1 = document.getElementById('Men');
-let saleman = document.getElementById('Man');
+const checkbox1 = document.getElementById('Men');
+const saleman = document.getElementById('Man');
+const checkbox2 = document.getElementById('Women');
+const salewoman = document.getElementById('Woman');
+const checkbox3 = document.getElementById('Children');
+const salechildren = document.getElementById('Сhildrenn');
 
-
-toggleFunc = function() {
+toggleFunс = function() {
 	if (!checkbox1.checked) {
 		saleman.style.display = 'none';
 	} else {
 		saleman.style.display = 'block';
 	}
-}
-
-checkbox1.addEventListener('change', (event) => {
-	toggleFunc();
-
-});
-
-toggleFunc();
-
-let checkbox2 = document.getElementById('Women');
-let salewoman = document.getElementById('Woman');
-
-toggleFun = function() {
 	if (!checkbox2.checked) {
 		salewoman.style.display = 'none';
 	} else {
 		salewoman.style.display = 'block';
 	}
 }
-
-checkbox2.addEventListener('change', (event) => {
-	toggleFun();
-});
-
-toggleFun();
-
-let checkbox3 = document.getElementById('Children');
-let salechildren = document.getElementById('childrenn');
-
-toggleFu = function() {
-	if (!checkbox3.checked) {
-		salechildren.style.display = 'none';
-	} else {
-		salechildren.style.display = 'block';
-	}
-}
-
-checkbox3.addEventListener('change', (event) => {
-	toggleFu();
-});
-
-toggleFu();
-
-
-// checkbox = {
-// 	name : 'men',
-// 	element : checkbox1,
-// 	toggleFunction : toggleFun,
-// };
-
-
-let button1 = document.getElementById('button1');
+checkbox1.addEventListener('change', (event) => {toggleFunс();});
+checkbox2.addEventListener('change', (event) => {toggleFunс();});
+checkbox3.addEventListener('change', (event) => {toggleFunс();});
+toggleFunс();
+const button1 = document.getElementById('button1');
 button1.addEventListener('click', () => {
-
-	// let checkboxesToToggle = [checkbox1, checkbox2];
-	// let checkboxesToToggleFunctions = [toggleFunc, toggleFun];
-	// let isEnableCheckboxes = false;
-
-	// for (let i in checkboxesToToggle) {
-	// 	if (!checkboxesToToggle[i].checked) {
-	// 		isEnableCheckboxes = true;
-	// 		break;
-	// 	}
-	// }
-
-	// for (let i in checkboxesToToggle) {
-	// 	checkboxesToToggle[i].checked = isEnableCheckboxes;
-	// 	checkboxesToToggleFunctions[i]();
-	// }
-
-
 
 	if (checkbox1.checked && checkbox2.checked && checkbox3.checked) {
 		checkbox1.checked = false;
 		checkbox2.checked = false;
 		checkbox3.checked = false;
-
-		toggleFunc();
-		toggleFun();
-		toggleFu();
+		toggleFunс();
+		
 	} else {
 		checkbox1.checked = true;
 		checkbox2.checked = true;
 		checkbox3.checked = true;
-
-		toggleFunc();
-		toggleFun();
-		toggleFu();
+		toggleFunс();
 	}
-
 });
 $(document).ready(function() {
     $('.down').click(function(){
