1f2e61bf09683f4ebb4972ec251980041f4ac820
phts1 <nsharp2111@gmail.com>
2019-02-06 18:40:24 +0300

    3.3.3

--- code.js
+++ code.js
@@ -4,48 +4,49 @@ const checkbox2 = document.getElementById('Women');
 const salewoman = document.getElementById('Woman');
 const checkbox3 = document.getElementById('Children');
 const salechildren = document.getElementById('Сhildrenn');
-
 const toggle = function(param) {
-if (Array.isArray(param)) {
-for (let i in param) {
-toggle(param[i]);
+        if (param.checkbox && param.checkbox.checked) {
+            param.elementToDisplay.style.display = 'block';
+        } else {
+            param.elementToDisplay.style.display = 'none';
+        }
 }
-
-return;
-}
-
-if (param.elementToDisplay !== undefined && param.elementToDisplay !== null) {
-if (param.checkbox && param.checkbox.checked) {
-param.elementToDisplay.style.display = 'block';
-} else {
-param.elementToDisplay.style.display = 'none';
-}
-}
-}
-
-let checkboxes = [
-{checkbox : checkbox1, elementToDisplay : saleman},
-{checkbox : checkbox2, elementToDisplay : salewoman},
-{checkbox : checkbox3, elementToDisplay : salechildren},
+const checkboxes = [{
+        checkbox: checkbox1,
+        elementToDisplay: saleman
+    },
+    {
+        checkbox: checkbox2,
+        elementToDisplay: salewoman
+    },
+    {
+        checkbox: checkbox3,
+        elementToDisplay: salechildren
+    },
 ];
-
-checkbox1.addEventListener('change', (event) => {toggle(checkboxes[0]);});
-checkbox2.addEventListener('change', (event) => {toggle(checkboxes[1]);});
-checkbox3.addEventListener('change', (event) => {toggle(checkboxes[2]);}); 
-
+checkbox1.addEventListener('change', (event) => {
+    toggle(checkboxes[0]);
+});
+checkbox2.addEventListener('change', (event) => {
+    toggle(checkboxes[1]);
+});
+checkbox3.addEventListener('change', (event) => {
+    toggle(checkboxes[2]);
+});
 const button1 = document.getElementById('button1');
 button1.addEventListener('click', () => {
-let isToggleOn = false;
-for (let i in checkboxes) {
-if (checkboxes[i].checkbox && !checkboxes[i].checkbox.checked) {
-isToggleOn = true;
-break;
-}
-}
-for (let i in checkboxes) {
-checkboxes[i].checkbox && (checkboxes[i].checkbox.checked = isToggleOn);
-}
-toggle(checkboxes);
+    let isToggleOn = false;
+    for (let i in checkboxes) {
+        if (!checkboxes[i].checkbox.checked) {
+            isToggleOn = true;
+            break;
+        }
+    }
+    for (let i in checkboxes) {
+        (checkboxes[i].checkbox.checked = isToggleOn);
+    }
+
+    for (let i in checkboxes) {
+		toggle(checkboxes[i]);
+    }
 });
-
-
